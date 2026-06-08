f45297c86a84c39b46ccedf908f0e50acedf9591
as4184 <125703547+as4184@users.noreply.github.com>
2026-06-08 12:48:14 -0400

    Update run_example.sh

--- examples/lung_nanopore/run_example.sh
+++ examples/lung_nanopore/run_example.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Run this from the repository root:
 # bash examples/lung_nanopore/run_example.sh
 
-bin/celltaminate run       --input-list examples/lung_nanopore/input_list.txt       --metadata examples/lung_nanopore/metadata.tsv       --outdir examples/lung_nanopore/results       --tax-level S       --ref-bg-tsv data/reference/refined_cell.lines.example.tsv
+bin/celltaminate run       --input-list examples/lung_nanopore/input_list.txt       --metadata examples/lung_nanopore/metadata.tsv       --outdir examples/lung_nanopore/results       --tax-level S       --ref-bg-tsv data/reference/refined_cell.lines.tsv
